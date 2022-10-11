fbf8568d424439fbac68e895defae0d062627d97
dallosnoel <71691760+dallosnoel@users.noreply.github.com>
2022-10-11 13:30:51 +0200

    Update 4.függvények.sql

--- 4.függvények.sql
+++ 4.függvények.sql
@@ -13,3 +13,51 @@ c,
 USE northwind;
 
 SELECT AVG(standard_cost) FROM products WHERE category = 'Sauces';
+
+-- 2.
+a,
+USE northwind;
+
+SELECT SUM(standard_cost) FROM products;
+
+b,
+USE northwind;
+
+SELECT SUM(standard_cost) FROM products WHERE list_price > 20 AND list_price < 50;
+
+c,
+USE northwind;
+
+SELECT SUM(standard_cost) FROM products WHERE category <> 'Sauces';
+
+-- 3.
+a,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
