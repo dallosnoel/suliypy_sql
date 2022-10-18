ac135d4ddc29353916023a411c6eae028c9fbba1
dallosnoel <71691760+dallosnoel@users.noreply.github.com>
2022-10-18 12:29:13 +0200

    Update 4.UNiON_GROUPBY_HAVING.sql

--- 4.UNiON_GROUPBY_HAVING.sql
+++ 4.UNiON_GROUPBY_HAVING.sql
@@ -1,1 +1,35 @@
+-- 1.
 
+USE northwind;
+
+  SELECT DISTINCT ship_state_province FROM orders
+  UNION
+  SELECT DISTINCT state_province FROM customers;
+ 
+ -- ismétlödő értékkel
+
+  SELECT  ship_state_province FROM orders
+  UNION ALL
+  SELECT  state_province FROM customers;
+  
+-- 2.
+
+USE northwind;
+
+SELECT COUNT(category) FROM products
+GROUP BY category;
+ 
+ -- 3.
+ 
+USE northwind;
+
+SELECT COUNT(ship_city) FROM orders
+GROUP BY ship_city;
+
+-- 4.
+
+USE northwind;
+
+SELECT COUNT(id), city FROM customers
+GROUP BY city
+HAVING COUNT(id) >= 3;
