dfb33cb865cd9131c99d4327288585ccfa439d2e
dallosnoel <71691760+dallosnoel@users.noreply.github.com>
2022-10-18 12:02:52 +0200

    Update 4.függvények.sql

--- 4.függvények.sql
+++ 4.függvények.sql
@@ -1,4 +1,5 @@
 -- 1.
+
 a, 
 USE northwind;
 
@@ -15,6 +16,7 @@ USE northwind;
 SELECT AVG(standard_cost) FROM products WHERE category = 'Sauces';
 
 -- 2.
+
 a,
 USE northwind;
 
@@ -31,13 +33,43 @@ USE northwind;
 SELECT SUM(standard_cost) FROM products WHERE category <> 'Sauces';
 
 -- 3.
+
 a,
 USE northwind;
 
 SELECT * FROM employees WHERE city = 'Seattle';
 
 b,
+USE northwind;
 
+SELECT * FROM employees WHERE job_title = 'Sales Representative';
+
+c,
+USE northwind;
+
+SELECT * FROM employees WHERE first_name LIKE 'A%';
+
+-- 4.
+
+a,
+USE northwind;
+
+SELECT MIN(standard_cost) FROM products;
+
+b,
+USE northwind;
+
+SELECT MAX(standard_cost) FROM products;
+
+c,
+USE northwind;
+
+SELECT MAX(standard_cost) FROM products WHERE list_price >= 30;
+
+d,
+USE northwind;
+
+SELECT MAX(standard_cost) FROM products WHERE product_code LIKE '%CO';
 
 
 
